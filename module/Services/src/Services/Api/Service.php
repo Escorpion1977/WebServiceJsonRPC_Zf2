4f413ea1ae0bbd1fe252258319c7f56d1888fe88
Flavio378 <flavio3_portela@hotmail.com>
2014-01-27 12:10:06 -0600

    codificar el resultado a json

--- module/Services/src/Services/Api/Service.php
+++ module/Services/src/Services/Api/Service.php
@@ -1,6 +1,8 @@
 <?php
 namespace Services\Api;
 
+use \Zend\Json\Json;
+
 class Service{
     
     /**
@@ -8,6 +10,6 @@ class Service{
      * @return array
      */
     public static function getRegistros(){
-        return array('1'=>'Flavio', '2'=>'Sergio', '3'=>'Luis');
+        return Json::encode(array('1'=>'Flavio', '2'=>'Sergio', '3'=>'Luis'));
     }
 }
